0a6511762ee223f9a4da5fa9c3a28349d279d84d
constab1e <armenat3@gmail.com>
2021-08-21 20:49:12 +0900

    basic auth and some endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TodoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+
+Route::get('/todos', [TodoController::class, 'index']);
+Route::get('/todos/{id}', [TodoController::class, 'getTodosById']);
+
+Route::middleware(['auth:sanctum', 'scope.user'])->group(function () {
+    Route::post('/todos', [TodoController::class, 'store']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::put('/todos/{id}', [TodoController::class, 'update']);
+
 });
+
+
